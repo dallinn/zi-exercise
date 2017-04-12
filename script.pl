f4761f2ce86be0b59aec33eaa138f7fbc4aed60f
dallinn <dallinn@users.noreply.github.com>
2017-04-11 23:47:29 -0600

    read_directory sub

--- script.pl
+++ script.pl
@@ -3,14 +3,58 @@
 use strict;
 use warnings;
 
+use Digest::SHA;
+
 __PACKAGE__->main;
 exit;
 
 sub main {
     my $self = shift;
+    my $directory;
+    my $url;
 
+     if (@ARGV <= 1) {
+        print STDERR "ERROR: Must be ran as the following:\n";
+        print "./script.pl DIRECTORY URL\n";
+        print "Example: ./script.pl ~/Code/ http://pastebin.com\n";
+        exit;
+    } else {
+        $directory = $ARGV[0] || die 'ERROR: please enter a directory as argument.';
+        $url       = $ARGV[1] || die 'ERROR: please enter a URL as argument.';
+    }
+
+    my @files = $self->read_directory($directory, $url);
+    die @files;
 }
 
 sub read_directory {
+    my ($self, $directory, $url) = @_;
+    my @files;
 
+    opendir (DIR, $directory) or die $!;
+
+    while (my $filename = readdir(DIR)) {
+        my $file = "$directory/$filename";
+        $file =~ s/\/\//\//g; # strip double slash '//'
+
+        if (-f $file) {
+            open(FILE, $file) or die "ERROR: $file could not be opened."; 
+            binmode(FILE); # Neccessary for proper sha256
+
+            # compute sha256sum
+            my $digest = Digest::SHA->new(256);
+            $digest->addfile(*FILE);
+            my $sha = $digest->hexdigest();
+
+            push @files, {
+                id   => $sha,
+                path => $file, 
+                size => -s $file,
+            };
+        }
+    }
+
+    closedir(DIR);
+
+    return @files; 
 }
